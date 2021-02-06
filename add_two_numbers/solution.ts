c20b80f48063aac78540f03b4f54612971b97866
jbeaken <jack747@gmail.com>
2021-02-06 13:51:54 +0000

    Solution now working as far as it will go, need better solution

--- add_two_numbers/solution.ts
+++ add_two_numbers/solution.ts
@@ -1,3 +1,22 @@
+import {ListNode} from './app/listnode'
+import {getListNodeFromNumber} from './app/listnode'
+
+// 342 and 465
+let num1 = getListNodeFromNumber( "342" )
+let num2 = getListNodeFromNumber( "465" )
+addTwoNumbers(num1, num2)
+
+// 942 and 9465
+num1 = getListNodeFromNumber( "942" )
+num2 = getListNodeFromNumber( "9465" )
+addTwoNumbers(num1, num2)
+
+
+//1000000000000000000000000000001 and 465
+num1 = getListNodeFromNumber( "1000000000000000000000000000001" )
+num2 = getListNodeFromNumber( "465" )
+addTwoNumbers(num1, num2)
+
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
    
     let current : ListNode | null = l1
@@ -53,3 +72,5 @@ function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | nul
      if (result == null) result = new ListNode(); //zero
      console.log( result )
     return result
+
+}
