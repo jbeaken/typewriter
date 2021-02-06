9fe52120572b086092178996a9afb6415f19576c
jbeaken <jack747@gmail.com>
2021-02-06 13:51:10 +0000

    Cleaning up template

--- add_two_numbers/solution.ts
+++ add_two_numbers/solution.ts
@@ -1,49 +1,55 @@
-/**
- * Definition for singly-linked list.
- * class ListNode {
- *     val: number
- *     next: ListNode | null
- *     constructor(val?: number, next?: ListNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.next = (next===undefined ? null : next)
- *     }
- * }
- */
-
-// 342 and 465
-var num1_1 = new ListNode(3, null);
-var num1_2 = new ListNode(4, num1_1);
-var num1_3 = new ListNode(2, num1_2);
-
-var num2_1 = new ListNode(4, null);
-var num2_2 = new ListNode(6, num1_1);
-var num2_3 = new ListNode(5, num1_2);
-
-addTwoNumbers(num1_3, num2_3)
-
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-
-    let current: ListNode | null = l1
-    
-    let numberAsStr1: string = ""
-    let numberAsStr2: string = ""
-
+   
+    let current : ListNode | null = l1
+    let sum1: string = ""
+    let sum2: string = ""
+ 
     while(current != null) {
-        console.log( current.val )
-        console.log( current.next )
-        numberAsStr1 += current.val;
-        current = current.next
+     sum1 = sum1 + current.val
+     current = current.next
     }
-
-    // current = l2
-
-    // while(current != null) {
-    //     numberAsStr2 += current.val;
-    //     current = current.next
-    // }
-
-    console.log( numberAsStr1)
-    // console.log( numberAsStr2)
-
-    return current
-};
+ 
+    //reverse
+    sum1 = sum1.split('').reverse().join('')
+ 
+    current = l2
+ 
+    while(current != null) {
+     sum2 = sum2 + current.val
+     current = current.next
+    }
+ 
+    sum2 = sum2.split('').reverse().join('')
+ 
+    console.log( "1st : " + sum1 )
+    console.log( "2nd : " + sum2 )
+ 
+    let total = Number(sum1) + Number(sum2)
+ 
+    console.log( "total : " + total )
+    
+    const chars = [...total.toString()];
+ 
+    let result : ListNode | null = null
+ 
+    //Argh!
+    //To prevent Variable 'dummyHead' is used before being assigned
+    let dummyHead: ListNode = new ListNode()
+ 
+    for(let c of chars.reverse()) {
+         let local : ListNode = new ListNode(Number(c))
+ 
+         if(result == null) {
+             //first character
+             result = local
+             dummyHead = result
+         } else {
+             dummyHead.next = local
+             dummyHead = dummyHead.next
+         }
+     }
+ 
+     //in case empty result
+     if (result == null) result = new ListNode(); //zero
+     console.log( result )
+    return result
